0d1c63f4dc7cfca4ea62bfd8ea0ead2466cf4e9d
Bhavika jain <jain.bhavika32@gmail.com>
2026-01-29 23:22:52 +0530

    Optimization

--- frontend/src/app/interceptors/jwt.interceptor.ts
+++ frontend/src/app/interceptors/jwt.interceptor.ts
@@ -38,31 +38,3 @@ export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
   );
 };
 
-@Injectable()
-export class JwtInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService, private router: Router) {}
-
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    const token = this.authService.getToken();
-    
-    if (token) {
-      request = request.clone({
-        setHeaders: {
-          Authorization: `Bearer ${token}`
-        }
-      });
-    } else {
-      console.log('JWT Interceptor (class) - No token found');
-    }
-
-    return next.handle(request).pipe(
-      catchError((error) => {
-        if (error.status === 401) {
-          this.authService.logout();
-          this.router.navigate(['/login']);
-        }
-        return throwError(() => error);
-      })
-    );
-  }
-}
