dae1659fb1de0ffeb86c46931c5ff284452400fe
Bhavika jain <jain.bhavika32@gmail.com>
2026-01-30 15:18:19 +0530

    No Auth Gaurd

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { authGuard } from './guards/auth.guard'
+import { noAuthGuard } from './guards/no-auth.guard';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent, canActivate: [noAuthGuard] },
   { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
   {
     path: 'products',
